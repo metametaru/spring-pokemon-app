627aff487d1b5b6849cf6d725aeee53b22135c44
metametaru <metametaru0812@gmail.com>
2026-02-05 15:52:38 +0900

    リリース対応

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import { Pokemon } from "@/src/types/pokemon";
+import FavoriteButton from "../FavoriteButton/FavoriteButton";
 import styles from "./Card.module.css";
 
 type Props = {
@@ -8,6 +9,9 @@ type Props = {
 const Card = ({ pokemon }: Props) => {
   return (
     <div className={styles.card}>
+      <div className={styles.header}>
+        <FavoriteButton pokemonId={pokemon.id} />
+      </div>
       <img
         className={styles.image}
         src={pokemon.image ?? ""}
@@ -16,8 +20,8 @@ const Card = ({ pokemon }: Props) => {
       <h3 className={styles.name}>{pokemon.name}</h3>
 
       <div className={styles.types}>
-        {pokemon.types.map((type) => (
-          <span key={type.name} className={`type ${type.name}`}>
+        {pokemon.types.map((type, index) => (
+          <span key={`${type.name}-${index}`} className={`type ${type.name}`}>
             {type.name}
           </span>
         ))}
@@ -32,8 +36,8 @@ const Card = ({ pokemon }: Props) => {
         <div className={styles.ability}>
           <p className="title">特性</p>
           <ul>
-            {pokemon.abilities.map((ability) => (
-              <li key={ability.name}>
+            {pokemon.abilities.map((ability, index) => (
+              <li key={`${ability.name}-${index}`}>
                 {ability.name}
                 {ability.isHidden && "（隠れ特性）"}
               </li>
